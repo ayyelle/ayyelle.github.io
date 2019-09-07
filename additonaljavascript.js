0a04999e14547362aaed8415e5cd07c65db03e1c
Anna Leong <aleong92@gmail.com>
2019-09-07 16:45:26 -0700

    Add files via upload

---
+++ additonaljavascript.js
@@ -0,0 +1,39 @@
+function showMoreInfo() {
+  var x = document.getElementById("long_intro");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById("other_experiences");
+  y.style.display = "none"
+  var z = document.getElementById("short_intro");
+  z.style.display = "none"
+}
+
+function showOtherExperiences() {
+  var x = document.getElementById("other_experiences");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById("long_intro");
+  y.style.display = "none"
+  var z = document.getElementById("short_intro");
+  z.style.display = "none"
+}
+
+
+function showSummary() {
+  var x = document.getElementById("short_intro");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById("long_intro");
+  y.style.display = "none"
+  var z = document.getElementById("other_experiences");
+  z.style.display = "none"
+}
