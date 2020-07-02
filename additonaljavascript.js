6d1e8467987cd7bab31e6501b7dcf356f93c05d3
Anna Leong <awyleong@interchange.ubc.ca>
2020-07-01 17:37:33 -0700

    Updating and adding new section to site

--- additonaljavascript.js
+++ additonaljavascript.js
@@ -2,22 +2,37 @@ function showMoreInfo() {
   var x = document.getElementById("long_intro");
   if (x.style.display === "none") {
     x.style.display = "block";
-  } else {
-    x.style.display = "none";
   }
+  var a = document.getElementById("technical_skills");
+  a.style.display = "none"
   var y = document.getElementById("other_experiences");
   y.style.display = "none"
   var z = document.getElementById("short_intro");
   z.style.display = "none"
 }
 
+function showTechnicalSkills() {
+  var x = document.getElementById("technical_skills");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } 
+  var y = document.getElementById("long_intro");
+  y.style.display = "none"
+  var z = document.getElementById("short_intro");
+  z.style.display = "none"
+  var a = document.getElementById("other_experiences");
+  a.style.display = "none"
+}
+
+
 function showOtherExperiences() {
   var x = document.getElementById("other_experiences");
   if (x.style.display === "none") {
     x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+  } 
+
+  var a = document.getElementById("technical_skills");
+  a.style.display = "none"
   var y = document.getElementById("long_intro");
   y.style.display = "none"
   var z = document.getElementById("short_intro");
@@ -29,9 +44,9 @@ function showSummary() {
   var x = document.getElementById("short_intro");
   if (x.style.display === "none") {
     x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+}
+  var a = document.getElementById("technical_skills");
+  a.style.display = "none"
   var y = document.getElementById("long_intro");
   y.style.display = "none"
   var z = document.getElementById("other_experiences");
